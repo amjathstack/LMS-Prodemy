268aa9fe283e0e211a5c17d91f4a8b46479d4d99
amjathstack <mohammedamjath772@gmail.com>
2026-01-04 11:18:33 +0530

    API fix

--- src/app/lib/features/coursesSlice.js
+++ src/app/lib/features/coursesSlice.js
@@ -2,7 +2,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-let URL = "/instructor/course";
+let URL = "/api/instructor/course";
 
 export const fetchInstructorCourses = createAsyncThunk(
     "courses/fetchInstructorCourses",
